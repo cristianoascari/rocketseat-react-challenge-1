2273f3d73e1e2ed8944b2a8116110c34e9cce57c
Cristiano Ascari <cristiano.ascari@gmail.com>
2024-02-13 09:46:39 -0300

    feat: no-tasks component created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
+import { useState } from 'react';
+
 import CreateTask from './components/create-task/create-task';
 import Header from './components/header/header';
+import NoTasks from './components/no-tasks/no-tasks';
+import Resume from './components/resume/resume';
+import Tasks from './components/tasks/tasks';
+
+import { TaskItem } from './shared/models/task.model';
 
 import './assets/styles/global.scss';
-import  './App.scss';
+import './App.scss';
 
 function App() {
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+
+  function handleCreateTask(task: TaskItem) {
+    setTasks([...tasks, task]);
+  }
+
+  /*function handleToggleTask(id: number) {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function handleRemoveTask(id: number) {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+  }*/
+
   return (
     <>
       <Header />
 
       <main>
-        <CreateTask />
+        <CreateTask
+          onCreateTask={handleCreateTask}
+        />
+
+        <Resume />
+
+        {!tasks.length && <NoTasks />}
+
+        {tasks.length > 0 && <Tasks />}
       </main>
     </>
   )
